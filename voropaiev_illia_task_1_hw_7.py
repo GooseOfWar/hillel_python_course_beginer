b6d741d564f248ec4f3546ef104ecf1a091ea2e2
GooseOfWar <marsealergei@gmail.com>
2022-06-01 11:25:34 +0300

    Update after Pylint, MyPy
    Checked code type hinting and tryed add more uderstandable code

--- voropaiev_illia_task_1_hw_7.py
+++ voropaiev_illia_task_1_hw_7.py
@@ -1,6 +1,6 @@
 """
-написать функцию которая в качестве аргумента принимает размер стороны квадрата, а возвращает кортеж в котором лежат
-три значения:
+написать функцию которая в качестве аргумента принимает размер стороны квадрата,
+а возвращает кортеж в котором лежат три значения:
 периметр квадрата
 диагональ квадрата
 площадь квадрата
@@ -9,21 +9,24 @@
 user_data: str = input("Fillup square side dimension in mm\n")  # data input
 
 
-def square_parameters(data):  # check data is.digit and calculate square parameters
+def square_parameters(data):
+    """Function check data is.digit and calculate square parameters"""
     while not data.isdigit():  # If data not only digits ask again
         data = input("Incorrect Data. Use only digit.\n")
-    else:  # if data is only digit: calculate perimetr diagonal and area of square
-        square_side = int(data)  # convert on int type
-        perimeter: int = square_side * 4    # perimeter calculation
-        diagonal: float = round(square_side / 0.707, 2)  # diagonal calculation
-        area: int = square_side ** 2    # area calculation
+    # if data is only digit: calculate perimetr diagonal and area of square
+    square_side = int(data)  # convert on int type
+    perimeter: int = square_side * 4    # perimeter calculation
+    diagonal: float = round(square_side / 0.707, 2)  # diagonal calculation
+    area: int = square_side ** 2    # area calculation
 
-     # tuple with square parameters
+    # tuple with square parameters
     square_parameters_data: tuple = (perimeter, diagonal, area)
     print(square_parameters_data)
-    
+
     #  Use f string for calculated data printing
-    print((f'Square Perimeter {perimeter} mm', f'Square diagonal {diagonal} mm', f'Square area {area} mm^2'))
+    print((f'Square Perimeter {perimeter} mm',
+           f'Square diagonal {diagonal} mm',
+           f'Square area {area} mm^2'))
     return square_parameters_data
 
 
--- voropaiev_illia_task_3_hw_7.py
+++ voropaiev_illia_task_3_hw_7.py
@@ -1,27 +1,30 @@
 """
-написать функцию, которая принимает в качестве аргументов два списка, а возвращает список,
-состоящий из элементов этих двух списков, при чем первый элемент списка - первый элемент первого аргумента,
-второй элемент списка - первый элемент второго списка, третий элемент - второй элемент первого списка,
+написать функцию, которая принимает в качестве аргументов два списка,
+а возвращает список, состоящий из элементов этих двух списков,
+при чем первый элемент списка - первый элемент первого аргумента,
+второй элемент списка - первый элемент второго списка,
+третий элемент - второй элемент первого списка,
 четвертый - второй элемент второго аргумента и т.д.
 т.е для аргументов [1, 2, 3] и [11, 22, 33] функция должна вернуть [1, 11, 2, 22, 3, 33].
 Будет плюсом использование генераторов последовательностей для решения этой задачи.
 """
+from typing import Union
 
 list_one: list = [j for j in range(1, 10)]  # first list generator
 list_two: list = [k for k in range(11, 100, 11)]  # second list generator
 
 
-def list_formatting(operation_list_1, operation_list_2):  # in first list input data from second list
-    if len(operation_list_1) != len(operation_list_2):  # compare length of list if they are not equal print massage
-        print('Lists length shall be equal!')
-    else:   # if length is OK insert data from second list in first list in position 1,3,5, etc
-        # second list index use division without remainder for iterator (1//2=0, 3//2=1, 5//2=2, etc)
-        # range shall be double length of first list for sufficient diapason
-        [operation_list_1.insert(i, operation_list_2[i // 2]) for i in range(1, (len(operation_list_2) * 2), 2)]
+def list_formatting(operation_list_1: list, operation_list_2: list) -> Union[list, str]:
+    """Input data from second list in first list"""
+    # if length is OK insert data from second list in first list in position 1,3,5, etc
+    # second list index use division without remainder for iterator (1//2=0, 3//2=1, 5//2=2, etc)
+    # range shall be double length of first list for sufficient diapason
+    if len(operation_list_1) == len(operation_list_2):
+        [operation_list_1.insert(i, operation_list_2[i // 2])
+        for i in range(1, (len(operation_list_2) * 2), 2)]
+        return operation_list_1
+    # compare length of list if they are not equal print massage
+    return 'Lists length shall be equal!'
 
-        print(operation_list_1)
-        
-        return opertion_list_1
 
-
-list_formatting(list_one, list_two)
+print(list_formatting(list_one, list_two))
