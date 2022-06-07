898692a70a477e5ede5c579c3047e4af0e4d694b
GooseOfWar <marsealergei@gmail.com>
2022-06-07 17:51:26 +0300

    Try work with pycharm JB

---
+++ HW_10/voropaiev_illia_task_4v2_hw_10.py
@@ -0,0 +1,84 @@
+"""
+Ваша задача - создать декоратор для функции, которая принимает
+неограниченное количество позиционных ХЕШИРУЕМЫХ элементов.
+
+Декоратор добавляет следующий функционал:
+Если функция уже вызвалась с такими аргументами - ваша функция должна вернуть результат
+выполнения этой функции из памяти, а не вычислять его заного.
+Если не вызывалась - вычислить результат, положить его в память, и вернуть.
+
+Подсказка - тут вам пригодятся словари.
+"""
+
+# TODO: я не делал проверку ввода на содержание букв так как при проверке
+#  дз показалось что это лишь лишний гемор для проверяющего
+#  так как в задании это нет.
+
+import json
+
+"""for hard memory realise use .json file
+operation_memory dict will be used for comparison new and existing data
+.json file use not use format tuple so need use two dict
+one dict (hard_memory) for rewriting json file, use str as a key
+next dict (operation_memory) use tuple format for operation in program"""
+
+with open('task4_10.json', "r") as json_file:
+    operation_memory: dict = json.load(json_file)
+    print(json.dumps(operation_memory, indent=2))  # show data in file
+
+# hard memory dict coping operation dict from .json file
+hard_memory: dict = operation_memory.copy()
+
+# Cycle use for program regular working
+while True:
+
+    # user input converted to the list with .split method
+    user_args: str = input("Input your args for summing\n")  # user args will be saved as a key for hard_memory dict
+    operation_args: list = user_args.split()    # operation list converted to the tuple and used for solving
+
+    if operation_args == ['q']:  # if input = q program will stop
+        print('Good bye!')
+        break
+
+    args_list = list(map(int, operation_args))   # arguments from input convert to the int
+    args_tuple: tuple = tuple(args_list)    # list with int args convert to the tuple because tuple is hashable
+
+
+    def check_agrs_decorator(fun):
+        """Function used like a decorator.
+        Compare arguments with previous arguments.
+        If arguments already exist function get value from memory dict.
+        In another case function use another function for solving sum"""
+
+        def wrapper(n_arg: tuple) -> int:
+            """Wrapper"""
+
+            if n_arg in operation_memory:
+                print("These arguments are already used".center(150, '_'))
+                return operation_memory[n_arg]
+
+            # from .json file operation dict also receives str keys
+            # because file read unformatted
+            # for comparison used user_args as a key(because they have same key format - str)
+            elif user_args in operation_memory:
+                print("These arguments are already used".center(150, '_'))
+                return operation_memory[user_args]
+
+            print("New args!".center(150, '_'))
+            return fun(n_arg)
+
+        return wrapper
+
+
+    @check_agrs_decorator
+    def hash_args(args: tuple) -> int:
+        """Function summing digital value and write value and results to the memory"""
+        operation_memory[args] = sum(args)
+        hard_memory[user_args] = sum(args)
+        return sum(args)
+
+
+    print(hash_args(args_tuple))
+
+with open('task4_10.json', "w") as json_file:  # write new and read data form hard_memory dict to the file
+    json.dump(hard_memory, json_file, indent=2)
