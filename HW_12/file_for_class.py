d92e7f1a3c2a2a031c461ed45931cb91e04e9399
GooseOfWar <marsealergei@gmail.com>
2022-06-15 13:06:39 +0300

    Use vars and locals() to see difference

--- HW_12/file_for_class.py
+++ HW_12/file_for_class.py
@@ -3,13 +3,17 @@ file for class in task 4
 """
 
 
+# create class
 class Visibility():
-    """Class used to return visibility area"""
+    """Class used to return visibility area and result of multiplication"""
+
     def __init__(self, val_1, val_2):
         self.mul = val_2 * val_1
 
+    # method that use locals to see local vars
     def local_vis(self):
         return locals()
 
+    # method that use vars to see local vars
     def vars_vis(self):
         return vars(self)
--- HW_12/voropaiev_illia_task_4_hw_12.py
+++ HW_12/voropaiev_illia_task_4_hw_12.py
@@ -6,10 +6,11 @@
 
 from file_for_class import Visibility
 
+# Create the class examples
 vis1 = Visibility(1, 2)
 vis2 = Visibility(2, 4)
 
-
+# print the result. Use locals and vars
 print(vis1, vis1.local_vis())
 print(vis2, vis2.vars_vis())
 
